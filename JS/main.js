5b20d3e5782559a46b0b7af2319a22f36b430a3b
AzamatAbraev <azamatabraev07@gmail.com>
2023-07-26 00:32:26 +0500

    JavaScript Exercises

---
+++ JS/main.js
@@ -0,0 +1,424 @@
+// Begin1
+
+// let a = +prompt("a tomoni");
+
+// let b = +prompt("b tomoni");
+
+// let S = a * b;
+
+// let P = 2 * (a + b);
+
+// console.log("Yuzasi =", S);
+
+// console.log("Perimetri =", P);
+
+
+
+// Begin2
+
+// let d = +prompt("Diametrini kiriting");
+
+// const PI = 3.14;
+
+// let L = d * PI;
+
+// let r = d / 2;
+
+// let S = PI * r ** 2;
+
+// console.log("Uzunligi =", L);
+
+// console.log("Aylananing Yuzasi =", S);
+
+
+
+// Begin3
+
+// let a = +prompt("Kubning yon tomonini kiriting");
+
+// let V = a ** 3;
+
+// let S = 6 * a ** 2;
+
+// console.log("Kubning hajmi =", V);
+
+// console.log("Kubning to`la sirti =", S);
+
+
+
+// Begin4
+
+// let a = +prompt("a tomonini kiriting");
+
+// let b = +prompt("b tomonini kiriting");
+
+// let c = +prompt("c tomonini kiriting");
+
+// let V = a * b * c;
+
+// let S = 2 * (a * b + b * c + a * c);
+
+// console.log("Paralelepipedning hajmi =", V);
+
+// console.log("Paralelepipedning to`la sirti =", S);
+
+
+
+// Begin5
+
+// let a = +prompt("a sonni kiriting");
+
+// let b = +prompt("b sonni kiriting");
+
+// let M = (a + b) / 2;
+
+// console.log("O`rta Arifmetigi =", M);
+
+
+
+// Begin6
+
+// let a = +prompt("a katet");
+
+// let b = +prompt("b katet");
+
+// let c = (a ** 2 + b ** 2) ** (1 / 2);
+
+// let P = a + b + c;
+
+// console.log("Gipotenuza =", c);
+
+// console.log("Perimetri =", P);
+
+
+
+// Begin7
+
+// let r1 = +prompt("radius 1");
+
+// let r2 = +prompt("radius 2");
+
+// const PI = 3.14;
+
+// r1 > r2;
+
+// let S1 = PI * r1;
+
+// let S2 = PI * r2;
+
+// S1 > S2;
+
+// let S = PI * (r1 ** 2 - r2 ** 2);
+
+// console.log("S1 Yuzasi =", S1);
+
+// console.log("S2 Yuzasi =", S2);
+
+// console.log(" Ularning ayirmasi =", S);
+
+
+
+// Begin8
+
+// let L = +prompt("Aylananing uzunligi");
+
+// const PI = 3.14;
+
+// let R = L / (PI * 2);
+
+// let S = PI * R ** 2;
+
+// console.log("Aylananing Radiusi =", R);
+
+// console.log("Aylananing Yuzasi =", S);
+
+
+
+// Begin9
+
+// let x1 = +prompt("x1 nuqtani kiriting");
+
+// let y1 = +prompt("y1 nuqtani kiriting");
+
+// let x2 = +prompt("x2 nuqtani kiriting");
+
+// let y2 = +prompt("y2 nuqtani kiriting");
+
+// let L = ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** (1 / 2);
+
+// console.log("L masofa =", L);
+
+
+
+// Begin10
+
+// let A = +prompt("A ning qiymati");
+
+// let B = +prompt("B ning qiymati");
+
+// let C = +prompt("C ning qiymati");
+
+// let a = A;
+
+// A = B;
+
+// let b = B;
+
+// B = C;
+
+// let c = C;
+
+// C = A;
+
+// console.log("A ning yangi qiymati =", b);
+
+// console.log("B ning yangi qiymati =", c);
+
+// console.log("C ning yangi qiymati =", a);
+
+
+
+// Begin11
+
+// let x = +prompt("x ning qiymati");
+
+// let y =  4 * (x - 3) ** 6 - 7 * (x - 3) ** 3 + 2;
+
+// console.log("y funksiyaning qiymati =", y);
+
+
+
+// Begin12
+
+// let A = +prompt("A ni kiriting");
+
+// let P = +prompt("A ning darajasini kiriting");
+
+// let C = A ** P;
+
+// console.log("Javob =", C);
+
+
+
+// Begin13
+
+// let Tf = +prompt("Temperatura (Farengeyt)");
+
+// let Tc = (Tf - 32) * (5/9);
+
+// console.log("Temperatura (Selsiy)", Tc);
+
+
+
+// Begin14
+
+// let X = +prompt("Shokolad: necha kg");
+
+// let A = +prompt("Necha so`m");
+
+// let Y = +prompt("Konfet: Necha kg");
+
+// let B = +prompt("Necha so`m");
+
+// let kgX1 = A / X;
+
+// let kgY1 = B / Y;
+
+// let diff = kgX1 - kgY1;
+
+// console.log("Narxdagi farqi =", diff);
+
+
+
+// Begin15
+
+// let X = +prompt("X kg");
+
+// let A = +prompt("Necha so`m");
+
+// let Y = +prompt("Y kg");
+
+// let B = +prompt("Necha so`m");
+
+// let totalAmount = (X * A) + (Y * B);
+
+// console.log("Umumiy narx =", totalAmount);
+
+
+
+
+// Integer1
+
+// let abc = +prompt("Uch xonali son kiriting");
+
+// let bc = abc % 100;
+
+// let c = bc % 10;
+
+// let b = (bc - c) / 10;
+
+// console.log("Bir xonali son =", c);
+
+// console.log("O`n xonali son =", b);
+
+
+
+// Integer2
+
+// let abc = +prompt("Uch xonali son kiriting");
+
+// let bc = abc % 100;
+
+// let c = bc % 10;
+
+// let b = (bc - c) / 10;
+
+// let a = (abc - bc) / 100;
+
+// let sum = a + b + c;
+
+// console.log("Raqamlar yig`indisi =", sum);
+
+
+
+// Integer3
+
+// let abc = +prompt("Uch xonali son kiriting");
+
+// let bc = abc % 100;
+
+// let c = bc % 10;
+
+// let b = (bc - c) / 10;
+
+// let a = (abc - bc) / 100;
+
+// let A = "" + a;
+
+// A = c;
+
+// let C = "" + c;
+
+// C = a;
+
+// let B = "" + b;
+
+// let reverse = A + B + C;
+
+// let reverseNumber = +reverse
+
+// console.log("Teskari tartibda yozilishi =", reverseNumber);
+
+// console.log(typeof reverseNumber);
+
+
+
+// Integer4
+
+// let abc = +prompt("Uch xonali son kiriting");
+
+// let bc = abc % 100;
+
+// let c = bc % 10;
+
+// let b = (bc - c) / 10;
+
+// let a = (abc - bc) / 100;
+
+// let A = "" + b;
+
+// let B = "" + a;
+
+// let C = "" + c;
+
+// let changed = A + B + C;
+
+// let changedNumber = +changed;
+
+// console.log("Natija =", changedNumber);
+
+
+
+// Integer5
+
+// let abc = +prompt("Uch xonali son kiriting");
+
+// let bc = abc % 100;
+
+// let c = bc % 10;
+
+// let b = (bc - c) / 10;
+
+// let a = (abc - bc) / 100;
+
+// let A = "" + a;
+
+// let B = "" + c;
+
+// let C = "" + b;
+
+// let changed = A + B + C;
+
+// let changedNumber = +changed;
+
+// console.log("Natija =", changedNumber);
+
+
+
+// Integer6
+
+// let abcd = +prompt("To`rt xonali son kiriting");
+
+// let bcd = abcd % 1000;
+
+// let a = (abcd - bcd) / 1000;
+
+// console.log("Minglar Xonasi =", a);
+
+
+// Integer7
+
+// let s = +prompt("Necha sekund o`tdi");
+
+// let leftSeconds = s % 60;
+
+// let minute = (s - leftSeconds) / 60;
+
+// console.log("Minut =", minute);
+
+
+// Integer8
+
+// let s = +prompt("Necha sekund o`tdi");
+
+// let leftSeconds = s % 3600;
+
+// let hour = (s - leftSeconds) / 3600;
+
+// console.log("Soat =", hour);
+
+
+// Integer9
+
+// let s = +prompt("Necha sekund o`tdi");
+
+// let leftSeconds = s % 60;
+
+// let minute = (s - leftSeconds) / 60;
+
+// console.log("Minut =", minute);
+
+// console.log("Sekund =", leftSeconds);
+
+
+// Integer10
+
+// let s = +prompt("Necha sekund o`tdi");
+
+// let leftSeconds = s % 3600;
+
+// let hour = (s - leftSeconds) / 3600;
+
+// console.log("Soat =", hour);
+
+// console.log("Sekund =", leftSeconds);
